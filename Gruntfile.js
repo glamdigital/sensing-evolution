b289643982ba23fdac752e18c57ec7d76f17a5f1
Andrew Haith <andyhaith@gmail.com>
2015-02-04 17:29:45 +0000

    Correct configuration of require.js and associated grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,6 @@ module.exports = function(grunt) {
     meta: {
       version: '0.1.0'
     },
-    bowerRequirejs: {
-      target: {
-        rjsConfig: 'app/config.js'
-      }
-    },
     banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '* http://PROJECT_WEBSITE/\n' +
@@ -81,6 +76,42 @@ module.exports = function(grunt) {
         files: '<%= jshint.lib_test.src %>',
         tasks: ['jshint:lib_test', 'qunit']
       }
+    },
+    "phonegap-build": {
+      release: {
+        options: {
+          archive: "app.zip",
+          "appID": "1274129",
+        }
+      }
+    },
+    compress: {
+      release: {
+        options: {
+          archive: "app.zip",
+          mode: "zip",
+        },
+        files: {}
+      }
+    },
+    requirejs: {
+      //release: {
+        compile: {
+          options: {
+            baseUrl: ".",
+            mainConfigFile: "app/require.config.js",
+            name: "bower_components/almond/almond.js", // assumes a production build using almond
+            out: "www/js/built.js",
+            //optimize: "none",
+            done: function(done, output) {
+              grunt.log.success(output);
+            },
+            include: ['app/main'],
+            //insertRequire: ['app/main']
+            //modules: [ { name: 'app/main' }],
+          }
+        }
+      //}
     }
   });
 
@@ -90,12 +121,14 @@ module.exports = function(grunt) {
   //grunt.loadNpmTasks('grunt-contrib-qunit');
   //grunt.loadNpmTasks('grunt-contrib-jshint');
   //grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-bower-requirejs');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-phonegap-build');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   // Default task.
-  grunt.registerTask('require', ['bowerRequirejs']);
   grunt.registerTask('style', ['compass']);
-  grunt.registerTask('package', ['bowerRequirejs', 'compass'])
+  grunt.registerTask('package', ['requirejs']);
+  grunt.registerTask('cloud', ['phonegap-build']);
 
 };
---
+++ app/require.config.js
@@ -0,0 +1,20 @@
+require.config({
+  baseURL: ".",
+  shim: {
+
+  },
+  paths: {
+    almond: "bower_components/almond/almond",
+    backbone: "bower_components/backbone/backbone",
+    fastclick: "bower_components/fastclick/lib/fastclick",
+    handlebars: "bower_components/handlebars/handlebars",
+    jquery: "bower_components/jquery/dist/jquery",
+    requirejs: "bower_components/requirejs/require",
+    underscore: "bower_components/underscore/underscore"
+  },
+  packages: [
+
+  ]
+});
+
+//require(['app/main']);
