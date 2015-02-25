545d1f54fe5eb1187be51da3057bc820f753a995
Andrew Haith <andyhaith@gmail.com>
2015-02-25 10:41:51 +0000

    Only show button to start trail once video is finished.

--- app/views/TrailIntroView.js
+++ app/views/TrailIntroView.js
@@ -9,10 +9,21 @@ define(["backbone", "app/models/Trail", "hbs!app/templates/trail_intro"],
             this.nextURL = params.nextURL;
         },
 
+        afterRender: function() {
+            this.$video = $('#intro-video');
+            this.video - this.$video[0];
+
+            this.$video.on('ended', this.showStartLink);
+        },
+
         serialize: function() {
             var out = this.trail.toJSON();
             out.nextURL = this.nextURL;
             return out;
+        },
+
+        showStartLink: function() {
+            $('.start-trail').show();
         }
 
     });
