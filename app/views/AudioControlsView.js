a0b2ba677f33ef12a2a55e554346a06c15790c29
Andrew Haith <andyhaith@gmail.com>
2015-03-26 13:00:57 +0000

    Audio playback now working on Android

--- app/views/AudioControlsView.js
+++ app/views/AudioControlsView.js
@@ -8,34 +8,52 @@ define(['backbone', 'hbs!app/templates/audio_controls'],
         initialize: function(params) {
             this.audio = params.audio;
             this.caption = params.caption;
+            //use Media plugin, for Android playback
+            if(typeof(Media) !== 'undefined') {
+                this.media_obj = new Media(this.getAudioURL(),
+                    //success
+                    function () {alert("Successfully created audio object");},
+                    //failure
+                    function (err) {
+                    alert("Failed to create audio object:" + err.code + " " + err.message );}
+                );
+            } else { alert("Media plugin not available!");}
         },
 
         serialize: function() {
             return { audio: this.audio, caption: this.caption };
         },
 
-        afterRender: function() {
-            this.$media = $('#media');
-            this.media = this.$media[0];
-        },
-
         events: {
           "click #play-audio" : "playAudio",
           "click #pause-audio" : "pauseAudio",
-          "click #restart-audio" : "restartAudio",
+          "click #restart-audio" : "restartAudio"
         },
 
-       playAudio: function(ev) {
-            if(this.media) {
-                this.media.play();
-                $('#play-audio').hide();
-                $('#pause-audio').show();
-                $('#restart-audio').show();
+        getAudioURL: function() {
+            path = 'audio/' + this.audio;
+            if(device.platform.toLowerCase() === "android") {
+                return "/android_asset/www/" + path;
             }
+            return path;
+        },
+       playAudio: function(ev) {
+            if(this.media_obj) {
+                this.media_obj.play();
+            }
+            else if(this.media) {
+                this.media.play();
+            }
+            $('#play-audio').hide();
+            $('#pause-audio').show();
+            $('#restart-audio').show();
         },
 
         pauseAudio: function(ev) {
-            if(this.media) {
+            if(this.media_obj) {
+                this.media_obj.pause();
+            }
+            else if(this.media) {
                 this.media.pause();
             }
             $('#play-audio').show();
@@ -43,11 +61,19 @@ define(['backbone', 'hbs!app/templates/audio_controls'],
         },
 
         restartAudio: function(ev) {
-            if(this.media) {
+            if(this.media_obj) {
+                this.media_obj.seekTo(0);
+            }
+            else if(this.media) {
                 this.media.currentTime = 0;
             }
         },
 
+        cleanup: function() {
+            if(this.media_obj) {
+                this.media_obj.release();
+            }
+        }
 
     });
 
