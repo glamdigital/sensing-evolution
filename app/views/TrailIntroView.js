514778acc2574d84ca5b5e0c68b1a21297313d4f
Andrew Haith <andyhaith@gmail.com>
2015-05-12 13:28:49 +0100

    Android video fixes

--- app/views/TrailIntroView.js
+++ app/views/TrailIntroView.js
@@ -23,13 +23,38 @@ define(["backbone", "underscore", "app/views/vcentre", "app/models/Trail", "hbs!
 		        });
 	        }
 
-            setTimeout(this.startVideo.bind(this), 5000);
+            //setTimeout(this.startVideo.bind(this), 5000);
+            this.startVideo();
+	        this.playing = false;
             this.$video.on('ended', this.showStartLink.bind(this));
+	        this.checkErrorTimeout = setTimeout(this.checkVideoError.bind(this), 500);
+	        //this.$video.on('canplaythrough', function(ev) {
+		     //   console.log("received canplay event");
+		     //   if(!this.playing) {
+			 //       console.log("Can play!");
+			 //       this.playing = true;
+			 //       this.startVideo();
+		     //   }
+	        //}.bind(this));
 
 			setTimeout(this.centreElements.bind(this), 100);
         },
+	    checkVideoError: function() {
+		    // Sometimes on Android, decoding fails at first attempt.
+		    // Check periodically and try to replay if it is in error state
+		    var video = this.$video[0];
+		    if(video.error !== null) {
+			    video.play();
+		    }
+		    this.checkErrorTimeout = setTimeout(this.checkVideoError.bind(this), 500);
+	    },
 	    centreElements: function() {
-	        this.moveToCentre(this.$video);
+		    //not on android - video has no height initially
+		    if(typeof(device)!='undefined') {
+			    if (device.platform !== 'Android' && device.platform !== 'amazon-fireos') {
+				    this.moveToCentre(this.$video);
+			    }
+		    }
 	    },
         startVideo: function() {
 	        console.log("Playing video");
@@ -46,6 +71,9 @@ define(["backbone", "underscore", "app/views/vcentre", "app/models/Trail", "hbs!
         },
 
         showStartLink: function() {
+	        //stop error checking
+	        clearTimeout(this.checkErrorTimeout);
+
 	        var $buttonsContainer = $('.buttons-container');
             $buttonsContainer.show();
             this.moveToCentre($buttonsContainer);
