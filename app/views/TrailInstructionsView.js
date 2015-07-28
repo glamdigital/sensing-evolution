aa0d1717b8774ada5de6b34cedd1e9edeb89881c
Andrew Haith <andyhaith@gmail.com>
2015-07-28 13:27:29 +0100

    Don't centre trail instructions

--- app/views/TrailInstructionsView.js
+++ app/views/TrailInstructionsView.js
@@ -18,17 +18,17 @@ define(['backbone', 'hbs!app/templates/trail_instructions', 'underscore', 'app/v
 
 		afterRender: function() {
 			//for some reason the dimensions aren't yet final in this function. Need a short delay.
-			setTimeout(this.centreContent.bind(this), 20);
+			//setTimeout(this.centreContent.bind(this), 20);
 		},
 
-	    centreContent: function() {
-		    this.moveToCentre($('.instructions'));
-	    }
+	    //centreContent: function() {
+		 //   this.moveToCentre($('.instructions'));
+	    //}
 
 
     });
 
-    _.extend(TrailInstructionsView.prototype, CentreMixin);
+    //_.extend(TrailInstructionsView.prototype, CentreMixin);
 
     return TrailInstructionsView;
 
